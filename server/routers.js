d1ba8ab1325e048dd21f8c10b180ab470717133e
eaTong <eatongchou@gmail.com>
2018-08-26 22:48:52 +0800

    bind user

--- server/routers.js
+++ server/routers.js
@@ -20,6 +20,7 @@ router.post('/api/*', structureData);
 
 router.post('/api/user/login', insertLog('login'), checkArguments(['account', 'password']), UserApi.login);
 router.post('/api/user/loginByCode', insertLog('login'), checkArguments(['code']), UserApi.loginByCode);
+router.post('/api/user/bind', insertLog('bind'), checkArguments(['code']), UserApi.bindUser);
 
 router.post('/api/image/upload', FileApi.uploadImage);
 
